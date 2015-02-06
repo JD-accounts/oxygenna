75fae40b04b78d2002d4845bfc94be56d5813986
John Langan <john.langan@oxygenna.com>
2015-02-06 15:01:34 +0200

    remove all materialAdmin names

--- app/gulp/build.js
+++ app/gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'materialAdmin'
+      module: 'triAngular'
     }))
     .pipe(gulp.dest(paths.tmp + '/partials/'));
 });
