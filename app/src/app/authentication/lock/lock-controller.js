f22ec2c1644368393c4582f8eb2288760d1bef06
John Langan <john.langan@oxygenna.com>
2015-02-09 15:23:50 +0200

    added lock screen

--- app/src/app/authentication/lock/lock-controller.js
+++ app/src/app/authentication/lock/lock-controller.js
@@ -12,38 +12,25 @@
  *
  */
 angular.module('triAngularAuthentication')
-.controller('LockController', function ($scope, $state, $timeout ) {
-    // $scope.user = AuthService.getUser();
-    // $scope.loginUser = {
-    //     email: $scope.user.email,
-    //     password: ''
-    // };
-    // // create array for alerts from form
-    // $scope.alerts = [];
+.controller('LockController', function ($scope, $state, $mdToast, $filter, AuthService) {
+    $scope.user = {
+        name: 'Morris Onions',
+        email: 'info@oxygenna.com',
+        password: 'demo'
+    };
 
-    // // controller to handle login check
-    // $scope.loginClick = function() {
-    //     AuthService.login($scope.loginUser, function(user) {
-    //         // user logged in ok so goto the dashboard
-    //         $state.go('private.admin.dashboard');
-    //     }, function(error) {
-    //         // username & password wrong so pop an error
-    //         $scope.loginUser.password = '';
-    //         $scope.showAlert({
-    //             title: 'Access Denied',
-    //             body: 'Invalid username and password - the password is demo if you forgot',
-    //             type: 'alert-danger'
-    //         });
-    //     });
-    // };
-
-    // $scope.showAlert = function(alert) {
-    //     $scope.alerts = [];
-    //     $scope.alerts.push(alert);
-    //     $timeout(function() {
-    //         $scope.$apply(function() {
-    //             $scope.alerts = [];
-    //         });
-    //     }, 4000);
-    // };
+    // controller to handle login check
+    $scope.loginClick = function() {
+        AuthService.login($scope.user, function() {
+            // user logged in ok so goto the dashboard
+            $state.go('private.admin.dashboard1');
+        }, function() {
+            $mdToast.show(
+                $mdToast.simple()
+                .content($filter('translate')('LOCK.MESSAGES.ACCESS_DENIED'))
+                .position('bottom right')
+                .hideDelay(5000)
+            );
+        });
+    };
 });
