3e762d2cc51b730b07300795cb869ea6867486f7
John Langan <john.langan@oxygenna.com>
2015-02-09 11:39:11 +0200

    tidy up translations in login

--- app/src/app/authentication/login/login-controller.js
+++ app/src/app/authentication/login/login-controller.js
@@ -32,36 +32,13 @@ angular.module('triAngularAuthentication')
         url: '#'
     }]
 
-    // create array for alerts from form
-    $scope.alerts = [];
-
     // controller to handle login check
     $scope.loginClick = function() {
         AuthService.login($scope.user, function(user) {
             // user logged in ok so goto the dashboard
             $state.go('private.admin.dashboard1');
         }, function(error) {
-            // username & password wrong so pop an error
-            $scope.user.password = '';
-            $scope.showAlert({
-                title: 'Access Denied',
-                body: 'Invalid username and password',
-                type: 'alert-danger'
-            });
+
         });
-    };
-
-    $scope.showAlert = function(alert) {
-        $scope.alerts = [];
-        $scope.alerts.push(alert);
-        $timeout(function() {
-            $scope.$apply(function() {
-                $scope.alerts = [];
-            });
-        }, 4000);
-    };
-
-    $scope.changeLanguage = function (langKey) {
-        $translate.use(langKey);
     };
 });
