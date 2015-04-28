438929a7525ab118faa89440a23153c516af4f59
John Langan <john.langan@oxygenna.com>
2015-04-28 08:58:45 +0300

    fix breadcrumb translations add Greek translation

--- app/src/components/toolbars/breadcrumb-directive.js
+++ app/src/components/toolbars/breadcrumb-directive.js
@@ -23,7 +23,7 @@ angular.module('triAngular')
         scope: {
             breadcrumb: '='
         },
-        template: '<span><span>{{breadcrumb.name}}<md-icon md-font-icon="icon-chevron-right" ng-show="breadcrumb.children.length > 0"></md-icon></span></span>',
+        template: '<span><span>{{breadcrumb.name | translate}}<md-icon md-font-icon="icon-chevron-right" ng-show="breadcrumb.children.length > 0"></md-icon></span></span>',
         link: function ($scope, $element, attrs) {
             if($scope.breadcrumb.children !== undefined) {
                 $element.find('span').attr('hide-sm', '');
