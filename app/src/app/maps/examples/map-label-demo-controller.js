87ed8796a5a5e4bab01c87165538c4467959a37e
Harris Kwnstantourakhs <harconst@gmail.com>
2015-05-11 17:16:10 +0300

    Google Maps Pages. Need more demos

---
+++ app/src/app/maps/examples/map-label-demo-controller.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name MapLabelDemoController
+ * @module triAngularMaps
+ * @kind function
+ *
+ *
+ */
+angular.module('triAngularMaps').
+controller('MapLabelDemoController', function ($scope, uiGmapGoogleMapApi) {
+    uiGmapGoogleMapApi.then(function(maps) {
+        $scope.labeledMap = {
+            center: {
+                latitude: 35.027469,
+                longitude: -111.022753
+            },
+            zoom: 4,
+            marker: {
+                id:0,                
+                coords: {
+                    latitude: 35.027469,
+                    longitude: -111.022753
+                },
+                options: {
+                    icon: {
+                        anchor: new maps.Point(36,36),
+                        origin: new maps.Point(0,0),                        
+                        url: 'assets/images/maps/blue_marker.png'
+                    }
+                }
+            }
+        };
+
+        $scope.labelTitle = "Hello from Arizona!";
+    });   
+});
---
+++ app/src/app/maps/maps.module.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name triangular.authentication
+ * @description
+ *
+ * The `triangular.authentication` module handles all the login and signup pages
+ */
+angular.module('triAngularMaps', ['uiGmapgoogle-maps'])
+.config(function ($translatePartialLoaderProvider, $stateProvider, uiGmapGoogleMapApiProvider) {
+    $translatePartialLoaderProvider.addPart('app/maps');
+
+    $stateProvider
+    .state('private.admin.toolbar.default.fullwidth', {
+        url: '/fullwidth',
+        templateUrl: 'app/maps/maps-fullwidth.tmpl.html',
+        controller: 'MapController'
+    })    
+    .state('private.admin.toolbar.default.demos', {
+        url: '/demos',
+        templateUrl: 'app/maps/maps-demo.tmpl.html',
+    });
+
+    uiGmapGoogleMapApiProvider.configure({        
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+})
+.run(function(SideMenu) {
+    SideMenu.addMenu({
+        name: 'MENU.MAPS.MAPS',
+        icon: 'icon-place',
+        type: 'dropdown',
+        priority: 7.1,
+        children: [{
+            name: 'MENU.MAPS.FULLWIDTH',
+            url: '/fullwidth',
+            type: 'link',
+        },{
+            name: 'MENU.MAPS.DEMOS',
+            url: '/demos',
+            type: 'link',
+        }]
+    });
+});
