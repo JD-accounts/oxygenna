e5f9e432720fa510dc6b5116d3b45a715fc1673b
John Langan <john.langan@oxygenna.com>
2015-06-04 16:34:34 +0300

    tidy up app.js module loading

--- app/src/app/app.js
+++ app/src/app/app.js
@@ -1,6 +1,13 @@
 'use strict';
 
-angular.module('triAngular', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ui.router', 'ngMaterial', 'pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'triAngularIntroduction', 'triAngularUI', 'triAngularAuthentication', 'triAngularDashboards', 'triAngularEmail', 'triAngularMenuLevels', 'triAngularElements', 'triAngularForms', 'triAngularCharts', 'triAngularMaps', 'triAngularExtras', 'triAngularTodo', 'ngMaterialDropmenu'])
+angular.module('triAngular', [
+    // inject angular modules
+    'ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngMaterial',
+    // inject extra 3rd party angular modules
+    'ui.router', 'pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'ngMaterialDropmenu',
+    // inject our own triangular modules
+    'triAngularIntroduction', 'triAngularUI', 'triAngularAuthentication', 'triAngularDashboards', 'triAngularEmail', 'triAngularMenuLevels', 'triAngularElements', 'triAngularForms', 'triAngularCharts', 'triAngularMaps', 'triAngularExtras', 'triAngularTodo'
+])
 .constant('APP', {
     name: 'triangular',
     logo: 'assets/images/logo.png',
