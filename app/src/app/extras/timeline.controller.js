c9073ec57c265fd8b15a55a6f6e51841bd0d8973
Harris Kwnstantourakhs <harconst@gmail.com>
2015-06-23 15:38:57 +0300

    avatars in timeline ( unstyled )

--- app/src/app/extras/timeline.controller.js
+++ app/src/app/extras/timeline.controller.js
@@ -12,30 +12,39 @@ angular.module('triAngularExtras').
 controller('TimelineController', function ($scope) {
     $scope.events = [{
         title: 'Carl Barnes',
-        date:'27/6/2015',        
+        date:'27/6/2015',
+        image: 'assets/images/avatars/hair-black-eyes-blue-green-skin-tanned.png'  
     },{
         title: 'Dorothy Lewis',
-        date:'27/6/2015',        
+        date:'27/6/2015',
+        image: 'assets/images/avatars/hair-black-eyes-brown-skin-tanned.png'
     },{
         title: 'Harris Kwnst',
         date:'26/6/2015',        
+        image: 'assets/images/avatars/hair-blonde-eyes-brown-skin-light.png'
     },{
         title: 'Sue Ross',
         date:'24/6/2015',        
+        image: 'assets/images/avatars/hair-black-eyes-dark-skin-dark.png'
     },{
         title: 'Joe Ross',
         date:'23/6/2015',        
+        image: 'assets/images/avatars/hair-blonde-eyes-blue-green-skin-light.png'
     },{
         title: 'Shirley King',
         date:'19/6/2015',        
+        image: 'assets/images/avatars/hair-blonde-eyes-brown-skin-tanned.png'
     },{
         title: 'John King',
         date:'17/6/2015',        
+        image: 'assets/images/avatars/hair-black-eyes-brown-skin-dark.png'
     },{
         title: 'Mary Rose',
         date:'16/6/2015',        
+        image: 'assets/images/avatars/hair-grey-eyes-dark-skin-tanned.png'
     },{
         title: 'Morris Onions',
         date:'12/6/2015',        
+        image: 'assets/images/avatars/hair-black-eyes-brown-skin-tanned-2.png'
     }];
 });
