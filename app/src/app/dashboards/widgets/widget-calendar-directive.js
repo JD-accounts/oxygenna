cf18bf44bad6f4b64aa34da0b3ed56b354885297
Harris Kwnstantourakhs <harconst@gmail.com>
2015-07-06 12:57:49 +0300

    Translate calendar module inside the directive link function ( because it comes from bower )

--- app/src/app/dashboards/widgets/widget-calendar-directive.js
+++ app/src/app/dashboards/widgets/widget-calendar-directive.js
@@ -16,7 +16,7 @@
 * ```
 */
 angular.module('triAngularDashboards')
-.directive('calendarWidget', function(uiCalendarConfig) {
+.directive('calendarWidget', function(uiCalendarConfig, $rootScope, $filter, $translate) {
     return {
         require: 'widget',
         restrict: 'A',
@@ -24,9 +24,16 @@ angular.module('triAngularDashboards')
             $scope.calendarEvents = [];
         },
         link: function($scope) {
+            // calendar strings to be translated
+            // calendar widget comes from bower, so we have to translate its strings in advance in the controller
+            $scope.dayNamesShort = ['DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.SUN', 'DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.MON', 'DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.TUE', 'DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.WED', 'DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.THU', 'DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.FRI', 'DASHBOARDS.WIDGETS.CALENDAR.DAYNAMESSHORT.SAT'];            
+            $scope.monthNames = ['DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.JANUARY','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.FEBRUARY','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.MARCH','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.APRIL','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.MAY','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.JUNE','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.JULY','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.AUGUST','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.SEPTEMBER','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.OCTOBER','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.NOVEMBER','DASHBOARDS.WIDGETS.CALENDAR.MONTHNAMES.DECEMBER'];
+
             $scope.calendarOptions = {
                 header: false,
-                height: 'auto',
+                height: 'auto',               
+                dayNamesShort : [],
+                monthNames: [],
                 viewRender: function(view) {
                     $scope.currentDay = view.calendar.getDate();
                 }
@@ -34,7 +41,29 @@ angular.module('triAngularDashboards')
 
             $scope.changeMonth = function(direction) {
                 uiCalendarConfig.calendars.calendarWidget.fullCalendar(direction);
+            };            
+
+            $scope.translateCalendar = function(){
+                $translate($scope.dayNamesShort).then(function (translations) {                    
+                    $scope.calendarOptions.dayNamesShort.length = 0;
+                    angular.forEach(translations, function(tr){
+                        $scope.calendarOptions.dayNamesShort.push(tr);
+                    });
+                });
+
+                $translate($scope.monthNames).then(function (translations) {                    
+                    $scope.calendarOptions.monthNames.length = 0;
+                    angular.forEach(translations, function(tr){
+                        $scope.calendarOptions.monthNames.push(tr);
+                    });
+                });
             };
+
+            $scope.translateCalendar();
+
+            $rootScope.$on('$translateChangeSuccess', function () {               
+                $scope.translateCalendar();
+            });            
         }
     };
 });
