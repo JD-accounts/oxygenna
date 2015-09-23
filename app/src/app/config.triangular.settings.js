13a195df153eb4e6c00612af248e355c1be5fabc
John Langan <john.langan@oxygenna.com>
2015-09-23 11:44:59 +0300

    version up 2.1.0

--- app/src/app/config.triangular.settings.js
+++ app/src/app/config.triangular.settings.js
@@ -11,7 +11,7 @@
         triSettingsProvider.setName('triangular');
         triSettingsProvider.setLogo('assets/images/logo.png');
         // set current version of app (shown in footer)
-        triSettingsProvider.setVersion('2.0.1');
+        triSettingsProvider.setVersion('2.1.0');
 
         // setup available languages in triangular
         for (var lang = APP_LANGUAGES.length - 1; lang >= 0; lang--) {
