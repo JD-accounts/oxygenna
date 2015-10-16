790fa721c370e2a3075685841ca7fcb1be1592e4
John Langan <john.langan@oxygenna.com>
2015-10-16 15:49:46 +0300

    bower build now minifying added seed app to extras updated changelog

--- app/gulp/bower.js
+++ app/gulp/bower.js
@@ -7,12 +7,10 @@ var $ = require('gulp-load-plugins')();
 
 
 gulp.task('bower', ['clean'], function () {
-    gulp.start('bower:build');
+  gulp.start('bower:build');
 });
 
-gulp.task('bower:build', ['bower:scripts', 'bower:styles'], function() {
-
-});
+gulp.task('bower:build', ['bower:scripts', 'bower:styles', 'bower:scripts:minify', 'bower:styles:minify']);
 
 gulp.task('bower:scripts', ['bower:partials'], function() {
   return gulp.src([
@@ -25,6 +23,24 @@ gulp.task('bower:scripts', ['bower:partials'], function() {
     .pipe(gulp.dest(paths.dist + '/'));
 });
 
+gulp.task('bower:scripts:minify', ['bower:scripts'], function() {
+  return gulp.src(paths.dist + '/**/*.js')
+    .pipe($.uglify())
+    .pipe($.rename(function (path) {
+      path.extname = '.min.js';
+    }))
+    .pipe(gulp.dest(paths.dist + '/'));
+});
+
+gulp.task('bower:styles:minify', ['bower:styles'], function() {
+  return gulp.src(paths.dist + '/**/*.css')
+    .pipe($.csso())
+    .pipe($.rename(function (path) {
+      path.extname = '.min.css';
+    }))
+    .pipe(gulp.dest(paths.dist + '/'));
+});
+
 var sassOptions = {
   style: 'expanded',
   includePaths: [
@@ -34,7 +50,6 @@ var sassOptions = {
 
 gulp.task('bower:styles', function() {
   return gulp.src(path.join(paths.src, '/app/triangular/triangular.scss'))
-    .pipe($.debug({title:'script'}))
     .pipe($.sass(sassOptions))
     .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']}))
       .on('error', function handleError(err) {
