3b305e1f72ff5e192e8c2a1944eda5c3ce9151fd
John Langan <john.langan@oxygenna.com>
2016-03-17 15:11:39 +0200

    remove calls to API in auth controllers

--- app/src/app/examples/authentication/signup/signup.controller.js
+++ app/src/app/examples/authentication/signup/signup.controller.js
@@ -6,7 +6,7 @@
         .controller('SignupController', SignupController);
 
     /* @ngInject */
-    function SignupController($scope, $state, $mdToast, $http, $filter, triSettings, API_CONFIG) {
+    function SignupController($scope, $state, $mdToast, $http, $filter, triSettings) {
         var vm = this;
         vm.triSettings = triSettings;
         vm.signupClick = signupClick;
@@ -20,30 +20,15 @@
         ////////////////
 
         function signupClick() {
-            $http({
-                method: 'POST',
-                url: API_CONFIG.url + 'signup',
-                data: vm.user
-            }).
-            success(function(data) {
-                $mdToast.show(
-                    $mdToast.simple()
-                    .content($filter('translate')('SIGNUP.MESSAGES.CONFIRM_SENT') + ' ' + data.email)
-                    .position('bottom right')
-                    .action($filter('translate')('SIGNUP.MESSAGES.LOGIN_NOW'))
-                    .highlightAction(true)
-                    .hideDelay(0)
-                ).then(function() {
-                    $state.go('authentication.login');
-                });
-            }).
-            error(function() {
-                $mdToast.show(
-                    $mdToast.simple()
-                    .content($filter('translate')('SIGNUP.MESSAGES.NO_SIGNUP'))
-                    .position('bottom right')
-                    .hideDelay(5000)
-                );
+            $mdToast.show(
+                $mdToast.simple()
+                .content($filter('translate')('SIGNUP.MESSAGES.CONFIRM_SENT'))
+                .position('bottom right')
+                .action($filter('translate')('SIGNUP.MESSAGES.LOGIN_NOW'))
+                .highlightAction(true)
+                .hideDelay(0)
+            ).then(function() {
+                $state.go('authentication.login');
             });
         }
     }
